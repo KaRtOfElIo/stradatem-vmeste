1df7ae91cf62fbd2db1bfe0f5fa7e245c6d4423c
KaRtOfElIo <164926717+KaRtOfElIo@users.noreply.github.com>
2024-08-26 23:17:36 +0300

    Update Mailing.py

--- Lesson3/Mailing.py
+++ Lesson3/Mailing.py
@@ -1,4 +1,6 @@
 class Mailing:
-    def __init__(self, cost, track):
-        self.cost = cost                  
-        self.track = track               
+    def __init__(self, to_address, from_address, cost, track):
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
